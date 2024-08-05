bb4bae8574035211fa22ccd75a3aef1df6979edd
CaptainEze <ezetheophilus05@gmail.com>
2024-08-05 10:07:54 +0100

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url : 'mongodb+srv://captEze:Tariq%40VENV200@cluster0.ze3tomg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', //mongo_url
+    url : process.evn.mongo, //mongo_url
     dbName : 'soc', // db_name
 }
